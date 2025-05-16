0d355b278376c2c1d2837f551eab4f5da023af88
Marcus Hare <marcus.hare@gmail.com>
2025-05-16 18:21:35 +0000

    Updates to test_my.py, uploaded unit_test.png

--- model_card_template.md
+++ model_card_template.md
@@ -3,16 +3,24 @@
 For additional information see the Model Card paper: https://arxiv.org/pdf/1810.03993.pdf
 
 ## Model Details
-
+The model was developed to predict whether a person makes more than $50,000 annually based on Census Data. 
+Model Version: 1.0
+Model type used: RandomForect Classifier
+Model Date: 05/16/2025
+Created By: Marcus Hare
 ## Intended Use
-
+The model is designed and intended to predict whether a person makes more than $50,000 annually using attributes provided by Census Data. This prediction can or will be used for research, academics, or decision making for marketing, planning, or possible eligibility.
 ## Training Data
-
+The Census Income Dataset was obtained as a .csv from the UCI Machine Learning Repository (https://archive.ics.uci.edu/ml/datasets/census+income)
 ## Evaluation Data
-
+The Census Income Dataset was also used for the evaluation, though it was between training and testing with 20% of the dataset designated for the testing/evaluation.
 ## Metrics
-_Please include the metrics used and your model's performance on those metrics._
-
+Three metrics were used to evaluate performance: Precision, Recall, and F1.
+Precision: Accuracy of positive predictions.
+Recall: Ability to capture the positive instances.
+F1: Balances both Precision and Recall.
+Performance based on the model: Precision: 0.7419 | Recall: 0.6384 | F1: 0.6863
 ## Ethical Considerations
-
+The dataset should only be considered a sample of the overall population and therefore cannot be a guaranteed overall representation. Biases and outliers may exists depending on the data captured and should be assessed for fairness when being used for decision making.
 ## Caveats and Recommendations
+The Dataset used is outdated and may not accurately reflect income levels for populations in present times. Recall metric also reflects an opportunity on identifying individuals with income over $50,000. Monitoring and retraining may be necessary to maintain accuracy.
--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,61 @@
 import pytest
 # TODO: add necessary import
-
+import numpy as np
+import pandas as pd
+from ml.data import process_data
+from ml.model import (
+    compute_model_metrics,
+    train_model, inference
+)
+from train_model import X_train, y_train
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import make_classification
 # TODO: implement the first test. Change the function name and input as needed
+
 def test_one():
     """
-    # add description for the first test
+    Verifying that the Model Type returns as expected.
     """
-    # Your code here
-    pass
-
+   # Create a control dataset
+    data = pd.DataFrame(
+        {
+            "feature1": [1, 2, 3, 4, 5],
+            "feature2": [0, 1, 0, 1, 0],
+            "label": [0, 1, 0, 1, 0],
+        }
+    )
+    # Process the data
+    X_train, y_train, _, _ = process_data(
+        data,
+        categorical_features=["feature2"],
+        label="label",
+        training=True,
+    )
+    # Train model
+    model = train_model(X_train, y_train)
+    # Verify model returned is expected.
+    expected_model_type = RandomForestClassifier
+    assert isinstance(model, expected_model_type)
 
 # TODO: implement the second test. Change the function name and input as needed
 def test_two():
     """
-    # add description for the second test
+    Verifying that metrics return and are between 0 and 1.
     """
-    # Your code here
-    pass
+    model = train_model(X_train, y_train)
+    preds = inference(model, X_train)
+    metrics = compute_model_metrics(y_train, preds)
+    assert len(metrics) == 3
+    assert type(metrics) == tuple
+    for metric in metrics:
+        assert metric >= 0 and metric <= 1
 
 
 # TODO: implement the third test. Change the function name and input as needed
 def test_three():
     """
-    # add description for the third test
+    Verifying that data loads and has the correct shape.
     """
-    # Your code here
-    pass
+    X, y = make_classification(n_samples=50, n_features=5, random_state=42)
+    assert X.shape == (50, 5)
+    assert y.shape == (50,)
